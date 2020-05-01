ad0fa3a148a2db4b3b33bb73da5c8b976ea6f507
FPLCodes <minecraftdudesh@gmail.com>
2020-05-01 21:00:04 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Pokedex from "./main/Pokedex";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Pokedex />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/main/Pokedex.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pokedex() {
+  return (
+    <div>
+      <h1>Pokedex</h1>
+    </div>
+  );
+}
