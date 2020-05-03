8d8824630c4d9b63580f0569f6d8a1273d2c10ab
FPLCodes <minecraftdudesh@gmail.com>
2020-05-03 11:12:45 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -5,25 +5,15 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { PokedexContainer } from "./container/PokedexContainer";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
   const client = new ApolloClient({
     uri: "https://graphql-pokemon.now.sh/",
   });
-
-  useEffect(() => {
-    setLoading(true);
-    setInterval(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
 
   return (
     <div>
       <ApolloProvider client={client}>
         <PokedexContainer />
       </ApolloProvider>
-      {loading && <h1>Loading...</h1>}
     </div>
   );
 }
--- src/container/PokedexContainer.js
+++ src/container/PokedexContainer.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { Pokemon } from "../components/Pokemon";
 import { GET_POKEMONS } from "../graphql/get-pokemons";
+import "../App.css";
 
 export function PokedexContainer() {
+  const [loading, setLoading] = useState(false);
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
     variables: { first: 151 },
   });
 
-  console.log(pokemons);
+  useEffect(() => {
+    setLoading(true);
+    setInterval(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="container">
-      {pokemons &&
-        pokemons.map((pokemon) => (
-          <Pokemon key={pokemon.id} pokemon={pokemon} />
-        ))}
+    <div>
+      {loading ? (
+        <div className="bg">
+          <img
+            src="https://pngimg.com/uploads/pokeball/pokeball_PNG8.png"
+            width="150px"
+            className="pokeball"
+          />
+        </div>
+      ) : (
+        <div className="container">
+          {pokemons &&
+            pokemons.map((pokemon) => (
+              <Pokemon key={pokemon.id} pokemon={pokemon} />
+            ))}
+        </div>
+      )}
     </div>
   );
 }
